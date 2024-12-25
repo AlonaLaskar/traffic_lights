f6efbcdb86af874c0a831bce3c59b62af716e7b7
Alona Laskar <alona159951@gmail.com>
2024-12-25 07:37:39 +0200

    Changing the path to TrafficLightState

--- lib/traffic_light_widget.dart
+++ lib/traffic_light_widget.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'traffic_light_control.dart';
+import 'traffic_light_modes.dart';
+
 
 class TrafficLight extends StatefulWidget {
   const TrafficLight({super.key});
